39c55c3d7c173d19c9b492797bd01708e50a5955
Manuel Ignacio López Quintero <miloqu@gmail.com>
2016-02-14 01:19:02 +0100

    Update apply_bin_mask_to_image.py

--- code/scripts/python/apply_bin_mask_to_image.py
+++ code/scripts/python/apply_bin_mask_to_image.py
@@ -3,10 +3,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 import scipy.io as sio
 
-image = plt.imread('00350a_img.jpg')
-bin_mask = sio.loadmat('00350a.mat')['person_segm'].astype('bool')
+image = plt.imread('input.png')
+bin_mask = sio.loadmat('mask.mat')['bin_mask'].astype('bool')
 
 mask = np.dstack((bin_mask, bin_mask, bin_mask))
 
 image[~mask] = 0
-plt.imsave('final.png', image)
+plt.imsave('output.png', image)
